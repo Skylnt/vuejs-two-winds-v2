5b055bfb30ef0382dafb49063be594ca46d9203c
HitItOff <tholn@tvccapital.com.vn>
2020-05-14 18:14:11 +0700

    chart

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import LandingPage from './views/LandingPage'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import Highcharts from 'highcharts'
+import HighchartsVue from 'highcharts-vue'
+import highchartsVariablePie from 'highcharts/modules/variable-pie'
+
+highchartsVariablePie(Highcharts);
+
+Vue.use(HighchartsVue, {
+	highcharts: Highcharts
+})
 
 Vue.config.productionTip = false
 
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -2,22 +2,87 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        <h1 class="lp__title">
-          Welcome to LandingPage
-        </h1>
+        <h1 class="lp__title">Welcome to LandingPage</h1>
+
+        <highcharts :options="chartOptions" :highcharts="hcInstance"></highcharts>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import Highcharts from "highcharts";
 export default {
   name: "LandingPage",
 
-  data: () => ({})
+  data: () => ({
+    hcInstance: Highcharts,
+    chartOptions: {
+      chart: {
+        type: "pie"
+      },
+      title: {
+        text: ""
+      },
+      credits: {
+        enabled: false
+      },
+      plotOptions: {
+        column: {
+          borderWidth: 5,
+          borderColor: {
+            linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+            stops: [
+              [0, "#1f4e94"], // start
+              [0.5, "#215598"], // middle
+              [1, "#318cb4"] // end
+            ]
+          }
+        },
+        pie: {
+          allowPointSelect: true,
+          cursor: "pointer",
+          colors: [
+            {
+              linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+              stops: [
+                [0, "#3290b6"], // start
+                [0.5, "#52a2b6"], // middle
+                [1, "#8bc1b3"] // end
+              ]
+            },
+            "#306aa5",
+            "#1ca6a9",
+            "#1db9cb"
+          ],
+          dataLabels: {
+            enabled: true,
+            format: "<p>{point.percentage:.1f} %</p>",
+            distance: -50
+          },
+          slicedOffset: 0
+        }
+      },
+      series: [
+        {
+          data: [
+            ["Firefox", 50],
+            ["IE", 10],
+            {
+              name: "Chrome",
+              y: 20,
+              sliced: true,
+              selected: true
+            },
+            ["Safari", 20]
+          ]
+        }
+      ]
+    }
+  })
 };
 </script>
 
 <style lang="scss">
-// @import "../assets/_lp.scss";
+@import "../assets/styles/_lp.scss";
 </style>
