e3917434b6bd27eeea4ac2e58a9d91f19b02d44f
HitItOff <tholn@tvccapital.com.vn>
2020-05-14 15:50:49 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import LandingPage from './views/LandingPage'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(LandingPage)
 }).$mount('#app')
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h1 class="lp__title">
+          Welcome to LandingPage
+        </h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+
+  data: () => ({})
+};
+</script>
+
+<style lang="scss">
+// @import "../assets/_lp.scss";
+</style>
